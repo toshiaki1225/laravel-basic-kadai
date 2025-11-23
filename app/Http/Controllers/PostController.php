bee8789422d2972f0a092c38be62793c26e0a42f
toshiaki1225 <th12250456135@outlook.com>
2025-11-23 11:07:04 +0900

    ローカルの変更を一時コミット

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+  namespace App\Http\Controllers;
+
+  use illuminate\Http\Request;
+
+  class PostController extends Controller{
+
+    public function index(){
+      return view('index');
+
+    }
+
+  }
+
+
+
+
+?>
